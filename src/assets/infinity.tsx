eebf7e150ae80024a36fa0cb2ebde2bad1d0948d
Simon Lake <simonl@theodo.co.uk>
2023-04-17 10:22:45 +0100

    feat: replace infinity svg

--- src/assets/infinity.tsx
+++ src/assets/infinity.tsx
@@ -6,7 +6,7 @@ export const InfinityScrollProgress: React.FC<{ progress: number }> = ({
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 14 8">
       <g>
         <path
-          d="M 3 0 C 1 0 0 1 0 3 C 0 5 1 6 3 6 C 6 6 6 0 9 0 C 11 0 12 1 12 3 C 12 5 11 6 9 6 C 6 6 6 0 3 0"
+          d="M 6 3 C 6 3 7 6 9 6 C 11 6 12 5 12 3 C 12 1 11 0 9 0 C 7 0 6 3 6 3 C 6 3 5 6 3 6 C 1 6 0 5 0 3 C 0 1 1 0 3 0 C 5 0 6 3 6 3"
           stroke="#000000"
           stroke-width="1"
           fill="none"
@@ -14,7 +14,7 @@ export const InfinityScrollProgress: React.FC<{ progress: number }> = ({
       </g>
       <g>
         <path
-          d="M 3 0 C 1 0 0 1 0 3 C 0 5 1 6 3 6 C 6 6 6 0 9 0 C 11 0 12 1 12 3 C 12 5 11 6 9 6 C 6 6 6 0 3 0"
+          d="M 6 3 C 6 3 7 6 9 6 C 11 6 12 5 12 3 C 12 1 11 0 9 0 C 7 0 6 3 6 3 C 6 3 5 6 3 6 C 1 6 0 5 0 3 C 0 1 1 0 3 0 C 5 0 6 3 6 3"
           stroke="#ffffff"
           stroke-width="1"
           fill="none"
